3d56f428b11d5ae241b2556137123dcf2c99a507
tenkco <sytnikkv@gmail.com>
2025-12-16 04:51:25 +0700

    the end

--- shopapp/forms.py
+++ shopapp/forms.py
@@ -5,32 +5,44 @@ from .models import Post, Comment, CustomUser
 from django.forms.models import inlineformset_factory
 
 class RegisterForm(forms.ModelForm):
-    password = forms.CharField(label="Пароль", widget=forms.PasswordInput)
-    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput)
+    password = forms.CharField(
+        label="Пароль",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label="Повтор пароля",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
     bio = forms.CharField(
         label="Биография",
-        widget=forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         required=False
     )
 
     def save(self, commit=True):
-        new_user = super().save(commit=False)
-        new_user.set_password(self.cleaned_data.get("password"))
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
         if commit:
-            new_user.save()
-        return new_user
+            user.save()
+        return user
 
     def clean(self):
-        super().clean()
-        pwd1 = self.cleaned_data.get("password")
-        pwd2 = self.cleaned_data.get("password2")
+        cleaned_data = super().clean()
+        pwd1 = cleaned_data.get("password")
+        pwd2 = cleaned_data.get("password2")
         if pwd1 and pwd2 and pwd1 != pwd2:
-            raise ValidationError({"password2": ValidationError("Пароли должны совпадать", code="password_mismatch")})
+            raise ValidationError({"password2": "Пароли должны совпадать"})
 
     class Meta:
         model = CustomUser
         fields = ("avatar", "first_name", "last_name", "username", "email", "bio")
-
+        widgets = {
+            'avatar': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
 class UserLoginForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите логин'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Введите пароль'}))
